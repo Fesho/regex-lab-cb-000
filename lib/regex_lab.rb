0b31d6458e7c4efb6e343839d41f980e9cb6876e
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-02 21:38:26 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,6 +3,9 @@ def starts_with_a_vowel?(word)
 word=["afoot", "Excellent", "incredible", "Absolute", "unreal", "Inconceivable","octopus" }.scan(/\b[aeiouAEIOU]/)
 
 end
+starts_with_a_vowel?(word)
+
+
 def words_starting_with_un_and_ending_with_ing(text)
 
 end
