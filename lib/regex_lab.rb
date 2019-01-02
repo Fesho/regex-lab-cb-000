3794f4d37ec2da8828b77096adaf4696331505ae
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-02 21:53:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,10 @@
 
 
 word =["afoot", "Excellent", "incredible", "Absolute", "unreal", "Inconceivable","octopus"]
-
+def
 word.grep(/\b[aeiouAEIOU]/)
+end
+
 def words_starting_with_un_and_ending_with_ing(text)
 
 end
