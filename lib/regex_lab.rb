607ebf7a8adfbc2c1369e53c426b2607c7cb81f2
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-02 21:27:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,5 @@
 def starts_with_a_vowel?(word)
-word="[afoot", "Excellent", "incredible", "Absolute", "unreal", "Inconceivable", "octopus"]
-end
+\b[aeiouAEIOU]/
 
 def words_starting_with_un_and_ending_with_ing(text)
 
